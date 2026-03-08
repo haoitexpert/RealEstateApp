d42c831375d01afff342d18d0bca77b6e759378d
HAO LUONG <luongvihao@outlook.com>
2026-03-08 18:00:55 +0700

    7.28_Service Class Method for Category

--- Services/ApiService.cs
+++ Services/ApiService.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Win32;
 using Newtonsoft.Json;
 using RealEstateApp.Models;
+using SoundAnalysis;
 using System;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace RealEstateApp.Services
@@ -41,12 +43,19 @@ namespace RealEstateApp.Services
             var response = await httpClient.PostAsync(AppSettings.ApiUrl + "api/users/login", content);
             if (!response.IsSuccessStatusCode) return false;
             var jsonResult = await response.Content.ReadAsStringAsync();
-            var result =  JsonConvert.DeserializeObject<Token>(jsonResult);
+            var result = JsonConvert.DeserializeObject<Token>(jsonResult);
             Preferences.Set("accesstoken", result.AccessToken);
             Preferences.Set("userid", result.UserId);
             Preferences.Set("username", result.UserName);
             return true;
         }
 
+        public static async Task<List<Category>> GetCategories()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Preferences.Get("accesstoken", string.Empty));
+            var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "api/categories");
+            return JsonConvert.DeserializeObject<List<Category>>(response);
+        }
     }
 }
