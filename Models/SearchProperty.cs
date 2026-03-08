8381e37205db9ca80a650dd64cfaf3b8a5747c76
HAO LUONG <luongvihao@outlook.com>
2026-03-08 11:31:44 +0700

    6.23_Create Model Classes For Properties Section

---
+++ Models/SearchProperty.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstateApp.Models
+{
+    public class SearchProperty
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("detail")]
+        public string Detail { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("imageUrl")]
+        public string ImageUrl { get; set; }
+
+        [JsonProperty("price")]
+        public int Price { get; set; }
+
+        [JsonProperty("isTrending")]
+        public bool IsTrending { get; set; }
+
+        [JsonProperty("categoryId")]
+        public int CategoryId { get; set; }
+
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+
+        [JsonProperty("bookmarks")]
+        public List<Bookmark> Bookmarks { get; set; }
+    }
+}
