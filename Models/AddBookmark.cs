6236437fa3b0624dd10b268434a387e766d45859
HAO LUONG <luongvihao@outlook.com>
2026-03-08 11:39:20 +0700

    6.24_Create Model Classed for Bookmarks Section

---
+++ Models/AddBookmark.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstateApp.Models
+{
+    public class AddBookmark
+    {
+        public int User_Id { get; set; }
+        public int PropertyId { get; set; }
+    }
+}
---
+++ Models/BookmarkList.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstateApp.Models
+{
+    public class BookmarkList
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("price")]
+        public int Price { get; set; }
+
+        [JsonProperty("imageUrl")]
+        public string ImageUrl { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("status")]
+        public bool Status { get; set; }
+
+        [JsonProperty("user_Id")]
+        public int UserId { get; set; }
+
+        [JsonProperty("propertyId")]
+        public int PropertyId { get; set; }
+    }
+}
