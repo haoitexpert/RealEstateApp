63659d1617827e9ceb1e56abbd7bf77a5bdcee70
HAO LUONG <luongvihao@outlook.com>
2026-03-10 19:51:09 +0700

    8.33_Implement Register Page Code Behind

--- Pages/RegisterPage.xaml.cs
+++ Pages/RegisterPage.xaml.cs
@@ -1,3 +1,5 @@
+using RealEstateApp.Services;
+
 namespace RealEstateApp.Pages;
 
 public partial class RegisterPage : ContentPage
@@ -6,4 +8,17 @@ public partial class RegisterPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    async void BtnRegister_Clicked(object sender, EventArgs e)
+    {
+		var response = await ApiService.RegisterUser(EntFullName.Text, EntEmail.Text, EntPassword.Text, EntPhone.Text);
+		if (response)
+		{
+			await DisplayAlert("", "Your account has been created", "Alright");
+		}
+        else
+        {
+            await DisplayAlert("", "An error occurred while creating your account", "Cancel");
+        }
+    }
 }
