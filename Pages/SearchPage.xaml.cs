775b1fcc6855777347743b914ae5fe08f98dcaa3
HAO LUONG <luongvihao@outlook.com>
2026-03-17 20:52:35 +0700

    14.51_Display Search Results

--- Pages/SearchPage.xaml.cs
+++ Pages/SearchPage.xaml.cs
@@ -1,3 +1,5 @@
+using RealEstateApp.Services;
+
 namespace RealEstateApp.Pages;
 
 public partial class SearchPage : ContentPage
@@ -6,4 +8,16 @@ public partial class SearchPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ImgBackBtn_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PopModalAsync();
+    }
+
+    private async void SbProperty_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if(e.NewTextValue == null) return;
+        var propertiesResult = await ApiService.FindProperties(e.NewTextValue);
+        CvSearch.ItemsSource = propertiesResult;
+    }
 }
